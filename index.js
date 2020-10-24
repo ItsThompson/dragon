b4567f6fe6de4352d453e6a0219d2e9def760b54
ItsThompson <33135895+ItsThompson@users.noreply.github.com>
2020-10-24 15:35:01 +0800

    ⚛️ Reaction Roles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const playerCount = require('./playerCount.js');
 // We dont need this.
 // const inVoice = require('./inVoice.js');
@@ -7,6 +7,10 @@ const register = require('./register.js');
 const streamVc = require('./streamVc.js');
 
 const prefix = process.env.PREFIX //drag! 
+
+let roleChannel = [];
+let roleId = [];
+let msgId = [];
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -16,6 +20,7 @@ client.on('ready', () => {
 client.on('message', msg => {
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
+    if(msg.author.bot) return;
 
     console.log(msg.content)
 
@@ -57,10 +62,86 @@ client.on('message', msg => {
             register(channel, amongUsName, twitch, discord, msg, client);
             break;
         }
+        case 'role':{
+            let title = args[0];
+            let role = args[1];
+            roleChannel.push(msg.channel.id);
+            roleId.push(args[1].substring(3).slice(0, -1));
+            let embed = new Discord.MessageEmbed()
+                .setTitle(title)
+                .setDescription(`React for ${role} role`)
+                .setColor('GREEN')
+
+            msg.channel.send(embed).then(function(msg){
+                msg.react('👍');
+                msgId.push(msg.id);
+            }).catch(function(error){
+                console.error(error);
+            });
+            break;
+        }
         default:
             break;
     }
 });
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.partial){
+        try {
+            await reaction.message.fetch();
+        } catch(error){
+            console.error(error)
+        }
+    }
+    if (reaction.partial){
+        try{
+            await reaction.fetch()
+        } catch(error){
+            console.error(error);
+            return;
+        }
+    }
+
+    for(let i = 0; i < roleChannel.length; i++){
+        if (reaction.message.channel.id === roleChannel[i] && reaction.message.id == msgId[i]){
+            if(reaction.emoji.name === '👍'){
+                await reaction.message.guild.members.cache.get(user.id).roles.add(roleId[i]);
+            }
+        }
+    }
+})
+
+client.on('messageReactionRemove', async (reaction, user) => {
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.partial){
+        try {
+            await reaction.message.fetch();
+        } catch(error){
+            console.error(error)
+        }
+    }
+    if (reaction.partial){
+        try{
+            await reaction.fetch()
+        } catch(error){
+            console.error(error);
+            return;
+        }
+    }
+
+    for(let i = 0; i < roleChannel.length; i++){
+        if (reaction.message.channel.id === roleChannel[i] && reaction.message.id == msgId[i]){
+            if(reaction.emoji.name === '👍'){
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(roleId[i]);
+            }
+        }
+    }
+})
 
 // we need to change this to an environment variable.
 client.login(process.env.BOT_TOKEN);
